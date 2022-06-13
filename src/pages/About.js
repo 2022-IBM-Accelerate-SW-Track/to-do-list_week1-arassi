0b7f35b34c3ee822aec8bf5d81982243f0cc1ae2
Anthony <arassi@usc.edu>
2022-06-13 20:56:34 +0300

    First version

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,10 +1,29 @@
 import React, { Component } from 'react';
+
+import "./About.css";
+import profile from "../assets/profile.jpeg";
   
 export default class About extends Component {
   render() {
     return (
       <div>
-        <p>Design your About me page </p> 
+        <div class="split left">
+          <div className="centered">
+            <img 
+              className="profile_image"
+              src={profile}
+              alt="Profile Pic"
+              ></img>
+          </div>
+        </div>
+        <div className="split right">
+          <div className="centered">
+            <div className="name_title">Anthony Rassi</div>
+            <div className="brief_description">
+              I'm a second-year Computer Engineering and Computer Science student.
+            </div>
+          </div>
+        </div>
       </div>
     )
   }
